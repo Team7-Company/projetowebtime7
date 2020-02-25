b7db97a5733a76f8456defb42c51b093671fa723
Ricardo <rcpompeo@gmail.com>
2020-02-25 03:01:58 -0300

    mudança de imagens no site

--- interacao.js
+++ interacao.js
@@ -1,1 +1,14 @@
-alert("oi")
+function imagem1(){
+    document.getElementById('id').src="img/pmc1.jpg";
+    setTimeout("imagem2()",3000)
+}
+
+function imagem2(){
+    document.getElementById('id').src="img/pmc2.png";
+    setTimeout("imagem3()",3000)
+}
+
+function imagem3(){
+    document.getElementById('id').src="img/pmc3.jpg";
+    setTimeout("imagem1()",3000)
+}

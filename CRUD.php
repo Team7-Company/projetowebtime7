eafd8988b3952f5b6e0c71d835680bd12e7b158e
natoo90 <61526436+natoo90@users.noreply.github.com>
2020-02-27 20:46:53 -0300

    Update CRUD.php

--- CRUD.php
+++ CRUD.php
@@ -2,31 +2,31 @@
 
 <?php
 
-include_once 'DataBase.php';
+include_once "DB.php";
 
 
-abstract class CRUD extends DataBase{
+abstract class CRUD extends DB{
     
     protected $tabela;
     
-    abstract public function insert();
+    abstract public function insert($nome,$cargo,$salario);
     abstract public function update($id);
     public function find($id){
         $sql = "Select * from $this->tabela where id = :id";
-        $stmt = DataBase::prepare($sql);
+        $stmt = DB::prepare($sql);
         $stmt->bindParam(':id',$id,PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetch();
     }
     public function findall(){
         $sql = "SELECT * FROM $this->tabela";
-        $stmt = DataBase::prepare($sql);
+        $stmt = DB::prepare($sql);
         $stmt->execute();
         return $stmt->fetch();
     }
     public function delete($id){
         $sql = "DELETE FROM $this->tabela WHERE ID = :id";
-        $stmt = DataBase::prepare($sql);
+        $stmt = DB::prepare($sql);
         $stmt->bindParam(':id',$id,PDO::PARAM_INT);
         return $stmt->execute();
     }

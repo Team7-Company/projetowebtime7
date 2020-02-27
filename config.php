46371cd585b7912e7ca400d66e85fe8b6b81f9e8
natoo90 <61526436+natoo90@users.noreply.github.com>
2020-02-27 20:47:12 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 <?php
 
 define("DB_HOST", "localhost");
-define("DB_NAME", “Time7”);
-define("DB_USER", “root”);
-define("DB_PASS", “root”);
+define("DB_NAME", "time7");
+define("DB_USER", "root");
+define("DB_PASS", "root");
 
 ?>

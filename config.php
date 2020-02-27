e88769f0a4d71ac202998f71766292d7b3ccebe3
natoo90 <61526436+natoo90@users.noreply.github.com>
2020-02-27 20:55:47 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+//Esta classe substitui os nomes para uso no MySQL
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "time7");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+
+?>
